37b18b3c25a7e4dc0839aca97c2548a96d0d5fa6
Aditri-Sures <96643325+Aditri-Sures@users.noreply.github.com>
2022-05-11 15:41:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:100
+});
+
+Webcam.attach("#Camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML="<img id='captured_image' src='"+data_uri+"'>";
+    });
+}
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/XqBCspYEh/model.json",modelLoaded);
+function modelLoaded(){
+    console.log("Model is loaded");
+}
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
